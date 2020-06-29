5f26210ba5bdc4845e236dce372e65101a1f29d5
Chris Beer <cabeer@stanford.edu>
2020-06-29 13:23:10 -0700

    Fix the prod build

--- app/main.dev.ts
+++ app/main.dev.ts
@@ -65,11 +65,9 @@ const createWindow = async () => {
       process.env.ERB_SECURE !== 'true'
         ? {
             nodeIntegration: true,
-            webSecurity: false
           }
         : {
             preload: path.join(__dirname, 'dist/renderer.prod.js'),
-            webSecurity: false
           },
   });
 
